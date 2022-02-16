5e5fe36390a6785e0f3a435a4ea94519670e460a
Laurent <laurent.rochette@codefresh.io>
2022-02-16 12:39:08 -0700

    running default pipleine thru the converter

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm -rf xxx
-python3 c2csdp.py test/demo2.yaml --ingress-host=https://lrcsdp.support.cf-cd.com \
+python3 c2csdp.py test/yeti3.yaml --ingress-host=https://lrcsdp.support.cf-cd.com \
   --log-level=debug
+python3 c2csdp.py test/demo2.yaml --ingress-host=https://lrcsdp.support.cf-cd.com
 python3 c2csdp.py test/demo1.yaml --ingress-host=https://lrcsdp.support.cf-cd.com
